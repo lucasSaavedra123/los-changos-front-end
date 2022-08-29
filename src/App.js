5c4fde91e15038e497b0c5f696e5b5f0c7764890
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-08-29 16:46:28 -0300

    new things

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
-import Login from './components/login.component'
-import SignUp from './components/signup.component'
+import SignUp from './Components/signup.component'
 
 
 function App() {
