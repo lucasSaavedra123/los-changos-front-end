c721b801fb8d4461839222256944aa1a031c95b1
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-09-02 19:36:37 -0300

    Things Deleted

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -2,9 +2,6 @@ import React from "react";
 
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import LandingPage from "./LandingPage";
-import "../../assets/scss/profile-menu.scss"
-import SideBar from "./Sidebar";
-import { Switch } from "react-router-dom";
 
 export const Profile = () => {
   const { user } = useAuth0();
@@ -15,23 +12,7 @@ export const Profile = () => {
   console.log("usuario:", user)
 
   return (
-
-    <Router id="Profile">
-      <SideBar pageWrapId={"page-wrap"} outerContainerId={"Profile"} />
-
-      <Switch>
-        <Route path="/SectionOne" component={Home}/>
-        <Route path="/SectionTwo" component={Settings}/>
-      </Switch>
-
-
-      <div id="page-wrap">
-        <Home/>
-        <Settings/>
-      </div>
-
-    </Router>
-
+    <div><button onClick={() => logout()}>Log Out</button> </div>
   );
 };
 
