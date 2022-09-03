b9989e3dd90597790cb9560fd99d4f8ebae1bb92
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-09-02 22:52:25 -0300

    Bar Added

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from "./LandingPage";
+
+import Home from './Sections/Home';
+import Reports from './Sections/Reports';
+import Products from './Sections/Products';
+
+import SideBar from '../Sidebar';
 
 export const Profile = () => {
   const { user } = useAuth0();
@@ -11,8 +18,23 @@ export const Profile = () => {
   console.log("autenticado:", isAuthenticated)
   console.log("usuario:", user)
 
+  /*
+  
+  <div><button onClick={() => logout()}>Log Out</button> </div>
+  
+  */
+
   return (
-    <div><button onClick={() => logout()}>Log Out</button> </div>
+    <>
+      <Router>
+        <SideBar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
