46a6b06aae9e9b310b5e14a6a29842a81f328be3
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-09-08 20:39:50 -0300

    prepare to change font

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -6,30 +6,45 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
+import Stack from 'react-bootstrap/Stack';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { CenterFocusStrong } from "@mui/icons-material";
 
 export const Navigator = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <img
-                            alt=""
-                            src="/logo512.png"
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />{' '}
-                        Walletify
-                    </Navbar.Brand>
-                    <Button variant="outline-success">Iniciar Sesion</Button>
+            <div class="landing-page">
+                <Navbar bg="black" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="#home">
+                            <img
+                                alt=""
+                                src="/logo512.png"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}
+                            Walletify
+                        </Navbar.Brand>
+                        <Button variant="outline-success">Iniciar Sesion</Button>
+                    </Container>
+                </Navbar>
+                <Container style={{
+                    display: "flex",
+                    width: "60%",
+                    justifyContent: "center",
+                    margin: "0 auto",
+                    color: "white"
+                }}>
+
+                    <Stack gap={3} style={{textAlign:"center"}}>
+                        <div><h1>Walletify</h1></div>
+                        <div><p>Nunca manejar la plata fue algo tan comodo.</p></div>
+                        <div><Button variant="success">Empezar ahora</Button></div>
+                    </Stack>
                 </Container>
-            </Navbar>
-            <Container>
-                Hola
-            </Container>
+            </div>
         </>
     );
 };
