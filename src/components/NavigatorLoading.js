0167844fbaf6b2624b2b85954d798f7f168f6f6a
Ignacio Tornati <nacho.tornati@gmail.com>
2022-09-12 19:04:36 -0300

    changes in loading indicator

---
+++ src/components/NavigatorLoading.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../assets/scss/landingPage.scss"
+import "../assets/scss/constants.scss"
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from '@mui/material/Button';
+import { THEME } from "../CONSTANTS"
+
+export const NavigatorLoading = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    return (
+        <>
+            <Navbar bg="black" variant="dark">
+                <Container style={{display:"flex"}}>
+                    <Navbar.Brand style={{marginLeft:"auto"}}>
+                        <img
+                            src="/logo192.png"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top logo"
+                            style={{"margin-right": "15px", "margin-left": "15px"}}
+                        />
+                        <span className="custom-font-light">Walletify</span>
+                    </Navbar.Brand>
+                </Container>
+
+            </Navbar>
+        </>
+    );
+};
+
+export default NavigatorLoading
--- src/components/views/LandingPage.js
+++ src/components/views/LandingPage.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "../../assets/scss/constants.scss"
 import "../../assets/scss/landingPage.scss"
 import { Navigator } from "../Navigator"
+import { NavigatorLoading } from "../NavigatorLoading"
 import { LandingPageFront } from "../LandingPageFront"
 import {Profile} from "./Profile";
 
@@ -12,7 +13,7 @@ import { SpinnerDiamond } from 'spinners-react';
 const LandingPage = () => {
 
     const { isAuthenticated } = useAuth0();
-    const isLoading = false ;//useAuth0().isLoading;
+    const isLoading = useAuth0().isLoading;
 
     const conditionalView = isAuthenticated ?
         <Profile/>
@@ -26,9 +27,12 @@ const LandingPage = () => {
 
     if(isLoading){
         return(
+            <>
+            <NavigatorLoading />
             <div className="center">
             <SpinnerDiamond size={80} enabled={true} secondaryColor="#FFFFFF" color="#A6FF00"/>
             </div>
+            </>
         )
     }else{
         return(conditionalView)
