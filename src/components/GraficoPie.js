aa00ca24fdd04962b482718767573de1a4909f52
nachotornati <nacho.tornati@gmail.com>
2022-10-16 10:46:22 -0300

    Dynamic Pie Chart

--- src/components/GraficoPie.js
+++ src/components/GraficoPie.js
@@ -5,59 +5,57 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
   export const GraficoPie = (props) =>{
 
+    const chartRef = useRef();
+    ChartJS.register(ArcElement, Tooltip, Legend);
+    const [total, setTotal] = useState();
+    const [labels, setLabels] = useState();
+    const [backgroundColors,setBackgroundColors] = useState();
+
     const getCategoriesFromTransactions = () =>{
       let categoriesValue = {}
-      let categoriesColor = []
+      let categoriesColor = {}
+      let categoriesName = {}
       props.transactions.map((transaction) => {
         categoriesValue[transaction.category.id] = typeof categoriesValue[transaction.category.id] === "undefined" ? transaction.value : categoriesValue[transaction.category.id] + transaction.value;
         categoriesColor[transaction.category.id] = transaction.category.color
+        categoriesName[transaction.category.id] = transaction.category.name
       })
+
+      let totalsAux = Object.values(categoriesValue)
+      let labelsAux = Object.values(categoriesName)
+      let backgroundColorsAux = Object.values(categoriesColor)
+
+      setTotal(totalsAux);
+      setLabels(labelsAux);
+      setBackgroundColors(backgroundColorsAux)
+      
     }
 
     useEffect(() => {
       getCategoriesFromTransactions()
     }, [props.transactions]);
-    
-    const newData = {
-      labels: ['Blue', 'Yellow', 'Green', 'Purple', 'Orange', "Prueba"],
-      datasets: [
-        {
-          data: [ 3, 5, 2, 3, 3,5],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderWidth: 1,
-        },
-      ],
-    };
 
-    const chartRef = useRef();
-    ChartJS.register(ArcElement, Tooltip, Legend);
-    const [data, setData] = useState(newData);
+
 
 
     const onClick = (event) => {
       let elements = getElementAtEvent(chartRef.current, event)
-      console.log(data.datasets.backgroundColor[elements[0].index]);
     }
 
-    
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          data: total,
+          backgroundColor: backgroundColors,
+          borderColor: backgroundColors,
+          borderWidth: 0.1,
+        },
+      ],
+    };
 
 
-    return <Pie ref={chartRef} options={{responsive:true}} data= {data} onClick={onClick} />
+    return (<Pie ref={chartRef} options={{responsive:true}} data= {data} onClick={onClick} />)
 }
 
 
